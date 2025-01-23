9dce38cef962d3a31515ab11a88fa6af369b6c84
HendrikdeLange <hendredl007@gmail.com>
2025-01-23 15:00:06 +0200

    Added avbob handling.

--- BANKS.py
+++ BANKS.py
@@ -1,4 +1,4 @@
-#import packages
+
 import streamlit as st
 import pandas as pd
 import os
@@ -15,7 +15,7 @@ file_list = []  # Will store text files
 df_masterfile = None  # Will hold the master Excel file data
 
 
-# Define your file processing function for Standard Bank
+
 def process_standard_bank_files(file_list, df_masterfile):
     for file in file_list:
         try:
@@ -29,9 +29,6 @@ def process_standard_bank_files(file_list, df_masterfile):
             df.columns = ['DATE', 'AMOUNT', 'DESCRIPTION']
             df['DESCRIPTION'] = df['DESCRIPTION'].str.strip()  # Remove leading/trailing spaces
 
-            print(f"Data after cleaning and renaming:")
-            print(df.head())  # Check the cleaned data
-
             # Add an auxiliary column for the original index
             df['original_index'] = df.index
 
@@ -44,11 +41,13 @@ def process_standard_bank_files(file_list, df_masterfile):
             def get_matching_code(line):
                 line = str(line).strip()  # Ensure it's a string and remove any surrounding spaces
 
-                # Pattern 12: 'DIV' followed by one or two numbers (anywhere in line, but not part of another word)
-                match12 = re.search(r'\bDIV(\d{1,2})\b', line)
-                if match12:
-                    code = match12.group(1)  # Extract only the number part
-                    return code
+                # Pattern 13: 'DIV' followed by one or two numbers (anywhere in line, but not part of another word)
+                match13 = re.search(r'\bDIV(\d{1,2})\b', line)
+                if match13:
+                    code = match13.group(1)  # Extract only the number part
+                    if code + ':' in line:  # Check if code is followed by ':'
+                        return None, None
+                    return code, 'pattern13'
 
                 # Pattern 6: 2 numbers, 3 letters, 4 numbers (start of line)
                 match6 = re.search(r'^\d{2}[A-Z]{3}\d{4}\b', line)
@@ -137,6 +136,14 @@ def process_standard_bank_files(file_list, df_masterfile):
                     if code + ':' in line:  # Check if code is followed by ':'
                         return None, None
                     return code, 'pattern11'
+
+                # Pattern 14: 4 letters, 4 numbers (start of line)
+                match14 = re.search(r'^\d[A-Z]{4}\d{4}\b', line)
+                if match14:
+                    code = match14.group(0)
+                    if code + ':' in line:  # Check if code is followed by ':'
+                        return None, None
+                    return code, 'pattern14'
 
                 # Pattern 12: 3 letters, 3 numbers (start of line)
                 match12 = re.search(r'^[A-Z]{3}\d{3}\b', line)
@@ -267,9 +274,9 @@ def process_absa_bank_files(file_list, df_masterfile):
                 line = str(line).strip()  # Ensure it's a string and remove any surrounding spaces
 
                 # Pattern 12: 'DIV' followed by one or two numbers (anywhere in line, but not part of another word)
-                match12 = re.search(r'\bDIV(\d{1,2})\b', line)
-                if match12:
-                    code = match12.group(1)  # Extract only the number part
+                match13 = re.search(r'\bDIV(\d{1,2})\b', line)
+                if match13:
+                    code = match13.group(1)  # Extract only the number part
                     return code
                 # Pattern 6: 2 numbers, 3 letters, 4 numbers (anywhere in line, but not part of another word)
                 match6 = re.search(r'(?<!\w)\d{2}[A-Z]{3}\d{4}(?!\w)', line)
@@ -335,6 +342,12 @@ def process_absa_bank_files(file_list, df_masterfile):
                 match11 = re.search(r'(?<!\w)[A-Z]{4}\d{2}(?!\w)', line)
                 if match11:
                     code = match11.group(0)
+                    return code
+
+                # Pattern 14: 4 letters, 4 numbers (start of line)
+                match14 = re.search(r'^\d[A-Z]{4}\d{4}\b', line)
+                if match14:
+                    code = match14.group(0)
                     return code
 
                 # Pattern 12: 3 letters, 3 numbers (anywhere in line, but not part of another word)
@@ -468,8 +481,12 @@ def process_capitec_bank_files(file_list):
     file_list.clear()
 
 
-# Streamlit UI code
+from pages import AVBOB  # Import your avbob module from the pages directory
+
+
+# Add navigation sidebar
 def main():
+
     st.markdown("""
         <style>
         .stApp {
@@ -491,23 +508,28 @@ def main():
         }
         </style>
     """, unsafe_allow_html=True)
-    st.markdown("""
-            <style>
-            .heading {
-                font-size: 36px;
-                font-weight: bold;
-                text-align: center;
-                color: #00008B;  /* Change to any color you want */
-            }
-            </style>
 
-            <div class="heading">BANK STATEMENT CONVERTER</div>
-        """, unsafe_allow_html=True)
+    st.markdown("""
+        <style>
+        .heading {
+            font-size: 36px;
+            font-weight: bold;
+            text-align: center;
+            color: #00008B;
+        }
+        </style>
+
+        <div class="heading">BANK STATEMENT CONVERTER</div>
+    """, unsafe_allow_html=True)
+
     std_bank = 'STANDARD BANK'
     abs_bank = "ABSA BANK"
     cpt_bank = 'CAPITEC BANK'
+
     # Select Bank Type
     bank_type = st.radio("", (std_bank, abs_bank, cpt_bank))
+
+    file_list = []
 
     # Upload Text File button
     uploaded_text_file = st.file_uploader("Upload Bank Statement", type=["txt", "csv", "xlsx"])
@@ -550,6 +572,7 @@ def main():
         elif bank_type == cpt_bank:
             if file_list:
                 process_capitec_bank_files(file_list)  # No need to check master file for CAPITEC
+
 
 if __name__ == "__main__":
     main()
---
+++ pages/AVBOB.py
@@ -0,0 +1,140 @@
+import pandas as pd
+import openpyxl
+import streamlit as st
+
+
+
+def process_employee_data(avbob_file, new_file, terminate_file):
+    # Read AVBOB data
+    df_avbob = pd.read_excel(avbob_file, engine="openpyxl")
+    df_avbob.columns = df_avbob.columns.str.strip()
+    avbob_columns = df_avbob.columns
+
+    # Read New Employee data
+    df_new_employees = pd.read_excel(new_file, engine="openpyxl")
+    df_new_employees.columns = df_new_employees.columns.str.strip()
+    add_columns = df_new_employees.columns
+
+    # Read Terminations data
+    df_terminations = pd.read_excel(terminate_file, engine="openpyxl")
+    terminations_columns = df_terminations.columns
+
+    # EMPLOYEE CODE HANDLING
+    og_employees = pd.Series(df_avbob[avbob_columns[0]])
+    new_employees = pd.Series(df_new_employees[add_columns[0]])
+
+    # Reset indices before concatenating
+    combined_employees = pd.concat([og_employees.reset_index(drop=True), new_employees.reset_index(drop=True)], axis=0)
+
+    # Initialize the new dataframe
+    df_new_avbob = pd.DataFrame(index=range(len(combined_employees)), columns=avbob_columns)
+
+    # Combine and process other columns
+    og_group = pd.Series(df_avbob[avbob_columns[3]])
+    new_group = pd.Series(df_new_employees[add_columns[6]])
+    combined_group = pd.concat([og_group.reset_index(drop=True), new_group.reset_index(drop=True)], axis=0)
+
+    df_new_employees[add_columns[8]] = df_new_employees[add_columns[8]].astype(str).str[:-2].astype(int)
+    og_commence = pd.Series(df_avbob[avbob_columns[4]])
+    new_commence = pd.Series(df_new_employees[add_columns[8]])
+    combined_commence = pd.concat([og_commence.reset_index(drop=True), new_commence.reset_index(drop=True)], axis=0)
+
+    og_surnames = pd.Series(df_avbob[avbob_columns[6]])
+    new_surnames = pd.Series(df_new_employees[add_columns[1]])
+    combined_surnames = pd.concat([og_surnames.reset_index(drop=True), new_surnames.reset_index(drop=True)], axis=0)
+
+    og_initials = pd.Series(df_avbob[avbob_columns[7]])
+    new_initials = pd.Series(df_new_employees[add_columns[2]])
+    combined_initials = pd.concat([og_initials.reset_index(drop=True), new_initials.reset_index(drop=True)], axis=0)
+
+    og_birth = pd.Series(df_avbob[avbob_columns[8]])
+    new_birth = pd.Series(df_new_employees[add_columns[4]])
+    combined_birth = pd.concat([og_birth.reset_index(drop=True), new_birth.reset_index(drop=True)], axis=0)
+
+    og_id = pd.Series(df_avbob[avbob_columns[9]])
+    df_new_employees[add_columns[3]] = df_new_employees[add_columns[3]].fillna(df_new_employees[add_columns[5]])
+    new_id = pd.Series(df_new_employees[add_columns[3]].astype(str))
+    combined_id = pd.concat([og_id.reset_index(drop=True), new_id.reset_index(drop=True)], axis=0)
+
+    og_gender = pd.Series(df_avbob[avbob_columns[11]])
+    new_gender = pd.Series(df_new_employees[add_columns[7]])
+    combined_gender = pd.concat([og_gender.reset_index(drop=True), new_gender.reset_index(drop=True)], axis=0)
+
+    # Reconstruct the new dataframe
+    df_new_avbob[avbob_columns[0]] = pd.Series(combined_employees.values)
+    df_new_avbob[avbob_columns[1]] = df_new_avbob[avbob_columns[1]].fillna("A")
+    df_new_avbob[avbob_columns[2]] = df_new_avbob[avbob_columns[2]].fillna(1284)
+    df_new_avbob[avbob_columns[3]] = pd.Series(combined_group.values)
+    df_new_avbob[avbob_columns[4]] = pd.Series(combined_commence.values)
+    df_new_avbob[avbob_columns[5]] = df_new_avbob[avbob_columns[5]].fillna(1)
+    df_new_avbob[avbob_columns[6]] = pd.Series(combined_surnames.values)
+    df_new_avbob[avbob_columns[7]] = pd.Series(combined_initials.values)
+    df_new_avbob[avbob_columns[8]] = pd.Series(combined_birth.values)
+    df_new_avbob[avbob_columns[9]] = pd.Series(combined_id.values)
+    df_new_avbob[avbob_columns[10]] = df_new_avbob[avbob_columns[10]].fillna("E")
+    df_new_avbob[avbob_columns[11]] = pd.Series(combined_gender.values)
+    df_new_avbob[avbob_columns[12]] = df_new_avbob[avbob_columns[12]].fillna(2000000)
+    df_new_avbob[avbob_columns[13]] = df_new_avbob[avbob_columns[13]].fillna("PO BOX 13596")
+    df_new_avbob[avbob_columns[14]] = df_new_avbob[avbob_columns[14]].fillna("NOORDSTAD")
+    df_new_avbob[avbob_columns[15]] = df_new_avbob[avbob_columns[15]].fillna("BLOEMFONTEIN")
+    df_new_avbob[avbob_columns[16]] = df_new_avbob[avbob_columns[16]].fillna(9302)
+    df_new_avbob[avbob_columns[21]] = df_new_avbob[avbob_columns[21]].fillna(514030400)
+
+    # TERMINATIONS
+    valid_terminations = df_terminations[terminations_columns[0]].dropna().unique()
+    df_new_avbob[avbob_columns[0]] = df_new_avbob[avbob_columns[0]].astype(str)
+    valid_terminations = valid_terminations.astype(str)
+
+    df_new_avbob[avbob_columns[0]] = df_new_avbob[avbob_columns[0]].str.strip()
+    valid_terminations = [code.strip() for code in valid_terminations]
+
+    # Remove rows with termination codes
+    df_new_avbob = df_new_avbob[~df_new_avbob[avbob_columns[0]].isin(valid_terminations)]
+
+
+    return df_new_avbob
+
+def main():
+    # Streamlit UI
+    st.markdown("""
+        <style>
+        .heading {
+            font-size: 36px;
+            font-weight: bold;
+            text-align: center;
+            color: green;
+        }
+        </style>
+
+        <div class="heading">AVBOB SCHEDULE</div>
+    """, unsafe_allow_html=True)
+
+    # File uploaders
+    avbob_file = st.file_uploader("Upload the previous month's schedule", type=["xlsx", "xls"])
+    new_file = st.file_uploader("Upload New Employees Data", type=["xlsx", "xls"])
+    terminate_file = st.file_uploader("Upload Terminations Data", type=["xlsx", "xls"])
+
+    # When the "Go" button is clicked
+    if st.button("Go"):
+        if avbob_file and new_file and terminate_file:
+            # Process the data
+            df_new_avbob = process_employee_data(avbob_file, new_file, terminate_file)
+
+            # Allow user to download the final processed file
+            output_file_path = "temp/final_output.xlsx"
+            df_new_avbob.to_excel(output_file_path, index=False)
+
+            # Provide download link
+            with open(output_file_path, "rb") as f:
+                st.download_button(
+                    label="Download Processed File",
+                    data=f,
+                    file_name="final_output.xlsx",
+                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                )
+        else:
+            st.error("Please upload all the required files before clicking 'Go'.")
+            st.write("Please ensure all uploaded files are uploaded as xlsx,with the newest excel engine")
+
+if __name__ == "__main__":
+    main()
